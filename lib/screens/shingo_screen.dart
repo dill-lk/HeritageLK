49561e9f9fb2feb6b226a231a4c271d0a71399eb
dill-lk <dill.ruzz.official@gmail.com>
2026-07-29 18:16:04 +0530

    fix: make shingo header decoration const

--- lib/screens/shingo_screen.dart
+++ lib/screens/shingo_screen.dart
@@ -350,10 +350,10 @@ class _ShingoScreenState extends State<ShingoScreen> {
               Container(
                 width: 32,
                 height: 32,
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                   color: Color(0x33E9C46A),
                   shape: BoxShape.circle,
-                  boxShadow: const [
+                  boxShadow: [
                     BoxShadow(
                       color: Color(0x4DE9C46A),
                       blurRadius: 10,
