54e1c56008e722cb441470b9340a93958511a94e
dill-lk <dill.ruzz.official@gmail.com>
2026-07-30 10:42:50 +0530

    fix: correct web photo uploader compilation

--- lib/screens/report_damage_photo_uploader_web.dart
+++ lib/screens/report_damage_photo_uploader_web.dart
@@ -39,31 +39,7 @@ Future<Uint8List> _readBytes(String photoPath) async {
   if (photoPath.startsWith('blob:')) {
     final response = await html.window.fetch(photoPath);
     final blob = await response.blob();
-    final reader = html.FileReader();
-    final completer = Completer<Uint8List>();
-    reader.onLoadEnd.listen((_) {
-      final result = reader.result;
-      if (result is ByteBuffer) {
-        completer.complete(Uint8List.view(result));
-      } else if (result is html.Blob) {
-        final reader2 = html.FileReader();
-        final completer2 = Completer<Uint8List>();
-        reader2.onLoadEnd.listen((_) {
-          final r2 = reader2.result;
-          if (r2 is ByteBuffer) {
-            completer2.complete(Uint8List.view(r2));
-          } else {
-            completer2.completeError('Unexpected FileReader result type');
-          }
-        });
-        reader2.readAsArrayBuffer(result);
-        return completer2.future;
-      } else {
-        completer.completeError('Unexpected FileReader result type');
-      }
-    });
-    reader.readAsArrayBuffer(blob);
-    return completer.future;
+    return _readBlob(blob);
   }
 
   final request = await html.HttpRequest.request(
@@ -73,55 +49,57 @@ Future<Uint8List> _readBytes(String photoPath) async {
   return Uint8List.view(request.response as ByteBuffer);
 }
 
+Future<Uint8List> _readBlob(html.Blob blob) async {
+  final reader = html.FileReader();
+  final completer = Completer<Uint8List>();
+  reader.onLoad.listen((_) {
+    final result = reader.result;
+    if (result is ByteBuffer) {
+      completer.complete(Uint8List.view(result));
+    } else {
+      completer.completeError('Unexpected FileReader result');
+    }
+  });
+  reader.onError.listen((_) => completer.completeError('Failed to read blob'));
+  reader.readAsArrayBuffer(blob);
+  return completer.future;
+}
+
 Future<Uint8List> _compressJpeg(Uint8List bytes) async {
   final blob = html.Blob([bytes]);
   final url = html.Url.createObjectUrlFromBlob(blob);
+
   final img = html.ImageElement();
   final completer = Completer<Uint8List>();
 
   img.onLoad.listen((_) {
-    final canvas = html.Canvas(width: img.width!, height: img.height!);
-    final ctx = canvas.context2D;
-    ctx.drawImageScaled(img, 0, 0, canvas.width!, canvas.height!);
+    try {
+      final maxWidth = 800;
+      final width = img.width!;
+      final height = img.height!;
+      var newWidth = width;
+      var newHeight = height;
 
-    final maxWidth = 800;
-    if (canvas.width > maxWidth) {
-      final ratio = maxWidth / canvas.width;
-      final newWidth = maxWidth;
-      final newHeight = (canvas.height * ratio).round();
-      final resized = html.Canvas(width: newWidth, height: newHeight);
-      resized.context2D.drawImageScaled(canvas, 0, 0, newWidth, newHeight);
-      resized.toBlob('image/jpeg', 0.75).then((blob) {
-        final reader = html.FileReader();
-        final c = Completer<Uint8List>();
-        reader.onLoadEnd.listen((_) {
-          final result = reader.result;
-          if (result is ByteBuffer) {
-            c.complete(Uint8List.view(result));
-          } else {
-            c.complete(bytes);
-          }
-        });
-        reader.readAsArrayBuffer(blob);
-        completer.complete(c.future);
-      }).catchError((_) => completer.complete(bytes));
-    } else {
-      canvas.toBlob('image/jpeg', 0.75).then((blob) {
-        final reader = html.FileReader();
-        final c = Completer<Uint8List>();
-        reader.onLoadEnd.listen((_) {
-          final result = reader.result;
-          if (result is ByteBuffer) {
-            c.complete(Uint8List.view(result));
-          } else {
-            c.complete(bytes);
-          }
-        });
-        reader.readAsArrayBuffer(blob);
-        completer.complete(c.future);
-      }).catchError((_) => completer.complete(bytes));
+      if (width > maxWidth) {
+        final ratio = maxWidth / width;
+        newWidth = maxWidth;
+        newHeight = (height * ratio).round();
+      }
+
+      final canvas = html.CanvasElement(width: newWidth, height: newHeight);
+      final ctx = canvas.context2D;
+      ctx.drawImageScaled(img, 0, 0, newWidth, newHeight);
+
+      final dataUrl = canvas.toDataURL('image/jpeg', 0.75);
+      final base64Str = dataUrl.split(',').last;
+      final compressed = base64Decode(base64Str);
+
+      html.Url.revokeObjectUrl(url);
+      completer.complete(compressed);
+    } catch (e) {
+      html.Url.revokeObjectUrl(url);
+      completer.complete(bytes);
     }
-    html.Url.revokeObjectUrl(url);
   });
 
   img.onError.listen((_) {
